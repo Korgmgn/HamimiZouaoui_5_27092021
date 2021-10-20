9bf52a347497e731e36d49ce420fcd7a9b5ef7a3
Hamimi Zouaoui <zouaoui.hm@gmail.com>
2021-10-20 11:56:56 +0200

    all functions done but loop bugged

--- front/js/product.js
+++ front/js/product.js
@@ -19,7 +19,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
         verifyColor()
         verifyQuantity()
-        addToCartClick()        
+        addToCartClick(data)        
     })
     .catch(function(error) {
         console.log('ERROR')
@@ -63,79 +63,57 @@ function displayItemColors(data){
 }
 
 //verifie si input couleur est valide
+
+let isColorTrue;
+
 function verifyColor(){
     colorSelect.addEventListener('change', function(e){
         console.log(e.target.value)
         if(e.target.value !== ""){
             console.log('couleur valide')
-            return true
+            isColorTrue = true
         } else {
             alert("Aucune couleur selectionnée")
-            return false
+            isColorTrue = false
         }
     })
 }
 
 //verifie si input quantité est valide
+
+let isQuantityTrue;
+
 function verifyQuantity(){
     itemQty.addEventListener('change', function(e) {
         console.log(e.target.value)
         if(e.target.value > 0 && e.target.value <= 100){
             console.log('quantité valide')
-            return true
+            isQuantityTrue = true
         } else {
             alert('La quantité doit être comprise entre 1 et 100')
-            return false
+            isQuantityTrue = false
         }
     })
 }
 
 //Déclenché au clic du bouton, vérifie d'abord que couleur & quantité sont valides
-function addToCartClick(){
+function addToCartClick(data){
     const itemToCart = document.getElementById('addToCart')
-    const cartExists = localStorage.getItem("itemsInCart")
 
     itemToCart.addEventListener('click', function(e){
-        if(verifyColor == true && verifyQuantity == true){
+        if(isColorTrue == true && isQuantityTrue == true){
             console.log('Tous les inputs sont valides')
-            checkForCart(cartExists)
+            checkForCart(data)
         } else {
-            console.log('Au moins un des inputs n\'est pas valide') //Même lorsque le console.log de verifyQuantity & verifyColor indiquent des valeures valides, cette fonction se termine ici 
+            console.log('Au moins un des inputs n\'est pas valide')
         }
     })
 }
 
 //Verifie si le localStorage existe
-function checkForCart(cartExists){
-    const parseCart = JSON.parse(cartExists)
+function checkForCart(data){
+    const cartExists = localStorage.getItem("itemsInCart")
 
-    if(cartExists !== null){
-        console.log('Le panier existe déjà')
-        searchCartForId(data, parseCart)
-    } else {
-        console.log('Le panier n\'existe pas')
-        pushItemToCart(data)
-    }
-}
-
-//Si le localStorage existe, vérifie si un produit avec le même Id && couleur existe
-function searchCartForId(data, parseCart){
-    for(let i = 0; i < parseCart.length; i++) {
-        const itemInCart = parseCart[i]
-        if(data._id == itemInCart.id && colorSelect.value == itemInCart.color){
-            itemInCart.quantity = itemInCart.quantity + itemQty.value //additionne les strings et non les nombres /!\
-            console.log('Cet objet existe déjà dans le panier')
-        } else {
-            console.log('Cet objet est n\'existe pas encore dans le panier')
-            pushItemToCart(data)
-        }
-    }
-}
-
-//Si le localStorage n'existe pas, l'objet produit et le localStorage sont créés
-function pushItemToCart(data){
-
-    let cart = []
     let addedItem = {
         id: data._id,
         name: data.name,
@@ -144,11 +122,54 @@ function pushItemToCart(data){
         altTxt: data.altTxt,
         color: colorSelect.value,
         quantity: itemQty.value
-    }
+    }    
 
-    cart.push(addedItem)
-    console.log(cart)
-    localStorage.setItem("itemsInCart", JSON.stringify(cart))
+    if(cartExists != null){
+        console.log('Le panier existe déjà')
+        searchCartForId(data, cartExists, addedItem)
+    } else {
+        console.log('Le panier n\'existe pas')
+        newLocalStorage(data, addedItem)
+    }
+}
+
+//Si le localStorage existe, vérifie si un produit avec le même Id && couleur existe
+function searchCartForId(data, cartExists, addedItem){
+    const parseCart = JSON.parse(cartExists)
+    let existingCart = parseCart
+    console.log(existingCart)
+
+    for(let i = 0; i < existingCart.length; i++) {
+        const itemInCart = existingCart[i]
+        if(data._id == itemInCart.id && colorSelect.value == itemInCart.color){
+            let newQuantityIncart  = Number(itemInCart.quantity) + Number(itemQty.value) 
+            itemInCart.quantity = newQuantityIncart
+            localStorage.setItem("itemsInCart", JSON.stringify(existingCart))
+            console.log('Cet objet existe déjà dans le panier, la quantité a été mise à jour')
+        } else {
+            console.log('Cet objet n\'existe pas encore dans le panier')
+            updateLocalStorage(existingCart, addedItem)        
+        }
+    }
+}
+
+//Constante pour la création d'objet à mettre dans l'array cart
+
+//Mets à jour le localStorage existant avec un nouvel objet
+function updateLocalStorage(existingCart, addedItem){
+    existingCart.push(addedItem)
+    localStorage.setItem("itemsInCart", JSON.stringify(existingCart))
+    console.log('Nouvel objet ajouté au panier existant')
+}
+
+//Si le localStorage n'existe pas, le premier objet et le localStorage sont créés
+function newLocalStorage(data, addedItem){
+
+    let newCart = []
+
+    newCart.push(addedItem)
+    localStorage.setItem("itemsInCart", JSON.stringify(newCart))
+    console.log('Panier créé et premier objet ajouté')
 }
 
 
